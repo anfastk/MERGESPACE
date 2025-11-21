2751c796e81a31e4cd0edbb56c1a226aec434734
anfastk13@gmail.com <anfastk13@gmail.com>
2025-11-21 12:19:16 +0530

    Misplaced repo deleted

--- internal/auth-service/domain/repository.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import "github.com/anfastk/MERGESPACE/internal/auth-service/domain/entity"
-
-type UserRepository interface {
-	Save(user *entity.User) error
-	FindByEmail(email string) (*entity.User, error)
-}
